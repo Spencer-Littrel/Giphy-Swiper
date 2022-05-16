b8642e8212b2910ff7762382ad107713ad3fb27d
Spencer-Littrel <95835247+Spencer-Littrel@users.noreply.github.com>
2022-05-16 13:53:48 -0700

    API, Links
    * Added API Key
    * Installed Swiper CSS and JS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*** Variables ***/
 // API Key
-const API_KEY = 'vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57';
+const API_KEY = 'XrMS8ayNnr3gE7rUp1VauwEvz3S6KzEz';
 // limit
-const limit = 25; 
+const limit = 25;
 // Get `input`
 const inputField = document.querySelector('.search-input');
 // Initialize or Default search query
@@ -10,42 +10,42 @@ inputField.value = '';
 
 // Listen to key presses
 inputField.addEventListener('keyup', event => {
-  
+
   if (event.key === 'Enter') {
-    
+
     // Go fetch Giphy API data
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${inputField.value}&limit=${limit}`)
-      .then( response => response.json() )
-      .then( gifs => {
-     
-      // check-check
-      console.log(gifs);
-      console.log(gifs.data);
-      
-      // Get container for data
-      const videoContainer = document.querySelector('.videos');
-      
-      // Loop through the array of data
-      gifs.data.forEach( gif => {
-        
+      .then(response => response.json())
+      .then(gifs => {
+
+        // check-check
+        console.log(gifs);
+        console.log(gifs.data);
+
+        // Get container for data
+        const videoContainer = document.querySelector('.videos');
+
+        // Loop through the array of data
+        gifs.data.forEach(gif => {
+
           // template 
-          const template  = `
+          const template = `
             <video src="${gif.images.original.mp4}" autoplay loop></video>
           `;
-        
+
           // append
           videoContainer.insertAdjacentHTML("afterbegin", template);
-        
+
+        });
+
+
       });
-      
-      
-    });
-    
+
     // Reset value and return cursor focus -- of input field
     inputField.value = '';
     inputField.focus();
-    
+
   }
-  
+
 });
 
